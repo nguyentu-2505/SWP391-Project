84d8fa8c13d36587258cdd649388f0a780d4913a
Trương Ngọc Bảo <truongbaongoc2006261@gmail.com>
2026-07-30 21:53:45 +0700

    man

--- backend/src/main/java/com/example/swp/features/team_invitation/TeamInvitationService.java
+++ backend/src/main/java/com/example/swp/features/team_invitation/TeamInvitationService.java
@@ -22,6 +22,7 @@ import com.example.swp.features.hackathon_event.HackathonStatus;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 @SuppressWarnings("null")
 public class TeamInvitationService {
 
